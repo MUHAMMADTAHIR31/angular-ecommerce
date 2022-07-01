16bce54a8c6f497882083809c1b8942123f93b2d
Muhammad Tahir <tahirsindhi872@gmail.com>
2022-07-01 23:58:48 +0500

    new changes

---
+++ src/app/components/product-details/product-details.component.ts
@@ -0,0 +1,35 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Product } from 'src/app/common/product';
+import { ProductService } from 'src/app/services/product.service';
+
+@Component({
+  selector: 'app-product-details',
+  templateUrl: './product-details.component.html',
+  styleUrls: ['./product-details.component.css']
+})
+export class ProductDetailsComponent implements OnInit {
+
+  product: Product= new Product();
+
+  constructor(private productService: ProductService,private router: ActivatedRoute) { }
+
+  ngOnInit(): void {
+    this.router.paramMap.subscribe(() => {
+      this.handleProductDetails();
+    }
+    );
+  }
+
+  handleProductDetails() {
+
+    // get the id parameter string.
+    const theProductId: number = Number(+this.router.snapshot.paramMap.get('id'));
+
+    this.productService.getProduct(theProductId).subscribe(   
+      data => { 
+        this.product = data;
+      }
+    );
+  }
+}
